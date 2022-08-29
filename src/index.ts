a65476b5f04dbd521bda2c23e8e43f6a1b0ffd15
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-08-29 17:35:42 +0200

    disabled local

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import express from 'express';
-import { appConfig } from './config';
-import { handler } from './lambda';
+// import express from 'express';
+// import { appConfig } from './config';
+// import { handler } from './lambda';
 
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-  console.log(req.method, res.status);
+// app.get('/', (req, res) => {
+//   console.log(req.method, res.status);
 
-  handler(
-    {
-      frequency: 1
-    },
-    undefined,
-    () => {}
-  );
-});
+//   handler(
+//     {
+//       frequency: 1
+//     },
+//     undefined,
+//     () => {}
+//   );
+// });
 
-app.listen(appConfig.express.port, () => {
-  console.log(
-    `App running under pid ${process.pid} and listening on port: ${appConfig.express.port} in ${appConfig.express.mode} mode`
-  );
-});
+// app.listen(appConfig.express.port, () => {
+//   console.log(
+//     `App running under pid ${process.pid} and listening on port: ${appConfig.express.port} in ${appConfig.express.mode} mode`
+//   );
+// });
 
 console.log('local startup');
